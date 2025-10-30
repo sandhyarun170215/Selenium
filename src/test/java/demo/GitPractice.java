5e1a4622a0b17db41e50333a3b817b27129e9964
sandhyarun170215 <lakshmiaj96@gmail.com>
2025-10-30 21:37:15 +0530

    changes three commit

--- src/test/java/demo/GitPractice.java
+++ src/test/java/demo/GitPractice.java
@@ -5,8 +5,7 @@ public class GitPractice {
 	public static void main(String[] args) {
 		System.out.println("hi");
 		System.out.println("Created");
-	  System.out.println("started");
-	  System.out.println("closed");
+	 System.out.println("hello");
 
 	}
 
